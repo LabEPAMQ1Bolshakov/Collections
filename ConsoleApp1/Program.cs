ee9311e15b47b51dc64ae364a8c7479d65dfa02c
Алексей Большаков <lexeybad@yandex.ru>
2018-04-19 22:25:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+            myDictionary.Add("Александр", 25);
+            myDictionary.Add("Петр", 35);
+            myDictionary.Add("Иван", 20);
+            myDictionary.Add("Семен", 15);
+            myDictionary.Add("Андрей", 5);
+            var result =
+                    // myDictionary.Keys - коллекция ключей, в нашем случае имён
+                from int age in myDictionary.Values
+                    // здесь по ключу получаем значение, т.е. возраст
+                where string name in myDictionary.Keys == "Иван"
+                    // сортируем по имени (по ключу)
+                orderby name
+                select name;
+
+            foreach (string name in result)
+                Console.WriteLine(name); // Выйдет Александр, Иван, Петр
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Women/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Women
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StudentGirl student = new StudentGirl();
+            student.Name = "Katty";
+            student.Age = 18;            
+            BusinessWoman bwoman = new BusinessWoman();
+            bwoman.Name = "Rachel";
+            bwoman.Age = 35;
+            HouseWife houseWife = new HouseWife();
+            houseWife.Name = "Sarah";
+            houseWife.Age = 28;
+            LittleGirl littleGirl = new LittleGirl();
+            littleGirl.Name = "Ann";
+            littleGirl.Age = 6;
+
+            List<Woman> ourWomen = new List<Woman>();
+            ourWomen.Add(student);
+            ourWomen.Add(bwoman);
+            ourWomen.Add(houseWife);
+            ourWomen.Add(littleGirl);
+
+            ourWomen.Add (new StudentGirl() { Name = "Triss", Age = 20});
+
+                //упорядочивание коллекции с помощью экземпляра класса WomanComparerByAge
+            WomanComparerByAge ageComparer = new WomanComparerByAge();
+            ourWomen.Sort(ageComparer);
+            Woman.showNameAgeFromList(ourWomen);
+            Console.Write("\n");
+
+                //запрос с использованием Lambda
+            Woman.selectYoungFromList(ourWomen);
+            Console.Write("\n");
+
+            //запрос с использованием LINQ
+            Woman.selectAdultFromList(ourWomen);
+                        
+            Console.ReadKey();
+            
+        }
+       
+    }
+}
